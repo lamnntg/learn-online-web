c3a996ab9c97b2c7942d387b14acef9a7606a624
lamylov3 <lamnguyen.hust@gmail.com>
2021-10-11 23:09:45 +0700

    add dispatcher

--- src/components/layout/Sidenav.js
+++ src/components/layout/Sidenav.js
@@ -164,7 +164,7 @@ function Sidenav({ color }) {
     <>
       <div className="brand">
         <img src={logo} alt="" />
-        <span>Muse Dashboard</span>
+        <span>Learn Online</span>
       </div>
       <hr />
       <Menu theme="light" mode="inline">
