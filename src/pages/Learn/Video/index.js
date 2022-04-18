4e0871f26aed33d72d3cc3060e82be3d42d7375a
HaMa05 <vanha.jungle@gmail.com>
2022-04-19 00:38:00 +0700

    mock url video

--- src/pages/Learn/Video/index.js
+++ src/pages/Learn/Video/index.js
@@ -1,127 +1,169 @@
-import React from 'react';
-import { Card, Col, Row, Typography, Tooltip, Progress, Upload, message, Button, Timeline, Radio } from 'antd';
-import { ToTopOutlined, MenuUnfoldOutlined, RightOutlined } from '@ant-design/icons';
-import Paragraph from 'antd/lib/typography/Paragraph';
+import { useState, useEffect } from 'react';
+import { Card, Col, Row, Typography, Timeline } from 'antd';
 
-const CourseVideo = () => {
-  const { Title, Text } = Typography;
-  const converPath = path => {
-    return path.replace('watch?v=', 'embed/');
-  };
+import './Video.style.scss';
 
-  const dummyData = [
-    {
-      id: 0,
-      title: 'Mô hình Client - Server là gì?',
-      url: 'https://youtu.be/CIIYogDrGto?list=PL_-VfJajZj0VkWYODGeMuraS8V7xaOZM8',
-      time: '10:30'
-    },
-    {
-      id: 1,
-      title: 'Domain là gì? Tên miền là gì? ',
-      url: 'https://youtu.be/XJiq_d0vGCQ?list=PL_-VfJajZj0VkWYODGeMuraS8V7xaOZM8',
-      time: '10:34'
-    }
-  ];
+function CourseVideo() {
+	const { Title, Text } = Typography;
 
-  return (
-    <>
-      {/* <Row className="h-full">
-        <Col span={18} className="h-full">
-          <Row className="h-full">
-            <iframe
-              width="100%"
-              height="70%"
-              src={converPath('https://www.youtube.com/watch?v=M62l1xA5Eu8')}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen></iframe>
-            <Row>
-              <h2 align="left" variant="h4">
-                Domain là gì? Tên miền là gì?
-              </h2>
-            </Row>
-          </Row>
-        </Col>
-        <Col span={6}>
-          <Card bordered={false} className="criclebox h-full">
-            <div className="timeline-box">
-              <Title level={5}>Orders History</Title>
-              <Paragraph className="lastweek" style={{ marginBottom: 24 }}>
-                this month <span className="bnb2">20%</span>
-              </Paragraph>
+	const dummyData = [
+		{
+			id: 0,
+			title: 'Lời khuyên trước khóa học',
+			url: 'https://www.youtube.com/watch?v=-jV06pqjUUc',
+			time: '10:30',
+		},
+		{
+			id: 1,
+			title: 'Cài đặt môi trường',
+			url: 'https://www.youtube.com/watch?v=efI98nT8Ffo',
+			time: '10:30',
+		},
+		{
+			id: 2,
+			title: 'Sử dụng JS trong file HTML',
+			url: 'https://www.youtube.com/watch?v=W0vEUmyvthQ',
+			time: '10:34',
+		},
+		{
+			id: 3,
+			title: 'Cài đặt môi trường',
+			url: 'https://www.youtube.com/watch?v=efI98nT8Ffo',
+			time: '10:30',
+		},
+		{
+			id: 4,
+			title: 'Sử dụng JS trong file HTML',
+			url: 'https://www.youtube.com/watch?v=W0vEUmyvthQ',
+			time: '10:34',
+		},
+		{
+			id: 5,
+			title: 'Cài đặt môi trường',
+			url: 'https://www.youtube.com/watch?v=efI98nT8Ffo',
+			time: '10:30',
+		},
+		{
+			id: 6,
+			title: 'Sử dụng JS trong file HTML',
+			url: 'https://www.youtube.com/watch?v=W0vEUmyvthQ',
+			time: '10:34',
+		},
+		{
+			id: 7,
+			title: 'Cài đặt môi trường',
+			url: 'https://www.youtube.com/watch?v=efI98nT8Ffo',
+			time: '10:30',
+		},
+		{
+			id: 8,
+			title: 'Sử dụng JS trong file HTML',
+			url: 'https://www.youtube.com/watch?v=W0vEUmyvthQ',
+			time: '10:34',
+		},
+		{
+			id: 9,
+			title: 'Cài đặt môi trường',
+			url: 'https://www.youtube.com/watch?v=efI98nT8Ffo',
+			time: '10:30',
+		},
+		{
+			id: 10,
+			title: 'Sử dụng JS trong file HTML',
+			url: 'https://www.youtube.com/watch?v=W0vEUmyvthQ',
+			time: '10:34',
+		},
+		{
+			id: 11,
+			title: 'Cài đặt môi trường',
+			url: 'https://www.youtube.com/watch?v=efI98nT8Ffo',
+			time: '10:30',
+		},
+		{
+			id: 12,
+			title: 'Sử dụng JS trong file HTML',
+			url: 'https://www.youtube.com/watch?v=W0vEUmyvthQ',
+			time: '10:34',
+		},
+		{
+			id: 13,
+			title: 'Cài đặt môi trường',
+			url: 'https://www.youtube.com/watch?v=efI98nT8Ffo',
+			time: '10:30',
+		},
+		{
+			id: 14,
+			title: 'Sử dụng JS trong file HTML',
+			url: 'https://www.youtube.com/watch?v=W0vEUmyvthQ',
+			time: '10:34',
+		},
+	];
+	const [course, setCourse] = useState(null);
 
-              <Timeline pending="Recording..." className="timelinelist">
-                {dummyData.map((t, index) => (
-                  <Timeline.Item key={index}>
-                    <Title level={5}>{t.title}</Title>
-                    <Text>{t.time}</Text>
-                  </Timeline.Item>
-                ))}
-              </Timeline>
-              <Button type="primary" className="width-100" onClick={() => setReverse(!reverse)}>
-                {<MenuUnfoldOutlined />} REVERSE
-              </Button>
-            </div>
-          </Card>
-        </Col>
-      </Row> */}
-      <Row gutter={[24, 0]} className="h-full">
-        <Col xs={24} sm={24} md={12} lg={12} xl={16} className="mb-24 h-full">
-          <div className="h-full">
-            {/* <div className="project-ant h-full">
-              <div>
-                <Title level={5}>Projects</Title>
-                <Paragraph className="lastweek">
-                  done this month<span className="blue">40%</span>
-                </Paragraph>
-              </div>
-              <div className="ant-filtertabs">
-                <div className="antd-pro-pages-dashboard-analysis-style-salesExtra"></div>
-              </div>
-            </div> */}
-            <div>
-              <Title level={5}>Domain là gì? Tên miền là gì?</Title>
-              <Paragraph className="lastweek">
-                done this month<span className="blue">40%</span>
-              </Paragraph>
-            </div>
-            <div className="h-full">
-              <iframe
-                width="100%"
-                height="70%"
-                src={converPath('https://www.youtube.com/watch?v=M62l1xA5Eu8')}
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen></iframe>
-            </div>
-          </div>
-        </Col>
-        <Col xs={24} sm={24} md={12} lg={12} xl={8} className="mb-24">
-          <Card bordered={false} className="criclebox h-full">
-            <div className="timeline-box">
-              <Title level={5}>Orders History</Title>
-              <Paragraph className="lastweek" style={{ marginBottom: 24 }}>
-                this month <span className="bnb2">20%</span>
-              </Paragraph>
+	useEffect(() => {
+		setCourse({ ...dummyData[0] });
+	}, []);
 
-              <Timeline pending="Recording..." className="timelinelist">
-                {dummyData.map((t, index) => (
-                  <Timeline.Item key={index}>
-                    <Title level={5}>{t.title}</Title>
-                    <Text>{t.time}</Text>
-                  </Timeline.Item>
-                ))}
-              </Timeline>
-              {/* <Button type="primary" className="width-100" onClick={() => setReverse(!reverse)}>
-              {<MenuUnfoldOutlined />} REVERSE
-            </Button> */}
-            </div>
-          </Card>
-        </Col>
-      </Row>
-    </>
-  );
-};
+	const chooseCourse = (course) => {
+		setCourse({ ...course });
+	};
+
+	const converPath = (path) => {
+		return path.replace('watch?v=', 'embed/');
+	};
+
+	return (
+		<>
+			<Row gutter={[24, 0]} className='h-full video'>
+				<Col xs={24} sm={24} md={12} lg={12} xl={16} className='mb-24 h-full video-left'>
+					<div className='h-full'>
+						{course && (
+							<>
+								<div>
+									<Title level={3}>{course.title}</Title>
+								</div>
+								<div className='h-full'>
+									<iframe
+										width='100%'
+										height='70%'
+										src={converPath(course.url)}
+										title='YouTube video player'
+										allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+										allowFullScreen></iframe>
+								</div>
+							</>
+						)}
+					</div>
+				</Col>
+				<Col xs={24} sm={24} md={12} lg={12} xl={8} className='mb-24 video-right'>
+					<Card bordered={false} className='criclebox h-full'>
+						<div className='timeline-box'>
+							<Title style={{ marginBottom: 24 }} level={5}>
+								Danh sách bài học
+							</Title>
+							<Timeline
+								pending='Đang tải khóa học...'
+								className='timelinelist'
+								style={{ height: '70vh', overflow: 'scroll', paddingTop: '10px' }}>
+								{course &&
+									dummyData.map((t, index) => (
+										<Timeline.Item key={index}>
+											<Title
+												className='cursor-pointer text-hover'
+												level={5}
+												style={{ color: t.id === course.id ? '#eb2f96' : '' }}
+												onClick={() => chooseCourse(t)}>
+												{index}. {t.title}
+											</Title>
+											<Text>{t.time}</Text>
+										</Timeline.Item>
+									))}
+							</Timeline>
+						</div>
+					</Card>
+				</Col>
+			</Row>
+		</>
+	);
+}
 export default CourseVideo;
-
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
-export const API_URL = 'http://localhost:8081/api/auth/';
-export const API_ROOT = 'http://localhost:8081/api';
+export const API_URL = 'http://localhost:8080/api/auth/';
+export const API_ROOT = 'http://localhost:8080/api';
 
 export const ROLE_ADMIN = 'ROLE_ADMIN';
 export const ROLE_MODERATOR = 'ROLE_MODERATOR';
 export const ROLE_USER = 'ROLE_USER';
-
