5eaf7123bcc7e145e9a4d9165e91d487cafe908e
HaMa05 <vanha.jungle@gmail.com>
2022-04-25 23:10:21 +0700

    update information

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,33 +1,41 @@
-import axios from "axios";
-import authHeader from "./auth-header";
-import { API_URL, API_ROOT } from "../utils/constants";
+import axios from 'axios';
+import authHeader from './auth-header';
+import { API_URL, API_ROOT } from '../utils/constants';
 
 const getPublicContent = () => {
-  return axios.get(API_URL + "all");
+	return axios.get(API_URL + 'all');
 };
 
 const getUserBoard = () => {
-  return axios.get(API_URL + "user", { headers: authHeader() });
+	return axios.get(API_URL + 'user', { headers: authHeader() });
 };
 
 const getModeratorBoard = () => {
-  return axios.get(API_URL + "mod", { headers: authHeader() });
+	return axios.get(API_URL + 'mod', { headers: authHeader() });
 };
 
 const getAdminBoard = () => {
-  return axios.get(API_URL + "admin", { headers: authHeader() });
+	return axios.get(API_URL + 'admin', { headers: authHeader() });
+};
+
+const updateUser = (id, data) => {
+	console.log(authHeader());
+	return axios.put(`${API_ROOT}/user/${id}/update`, data, { headers: authHeader() });
 };
 
 const uploadAvatar = async (data) => {
-  const req = await axios.post(`${API_ROOT}/user/avatar/update`, data, { headers: { ...authHeader(), 'Content-Type': 'application/json' } });
+	const req = await axios.post(`${API_ROOT}/user/avatar/update`, data, {
+		headers: { ...authHeader(), 'Content-Type': 'application/json' },
+	});
 
-  return req.data;
+	return req.data;
 };
 
 export const userService = {
-  getPublicContent,
-  getUserBoard,
-  getModeratorBoard,
-  getAdminBoard,
-  uploadAvatar
+	getPublicContent,
+	getUserBoard,
+	getModeratorBoard,
+	getAdminBoard,
+	uploadAvatar,
+	updateUser,
 };
