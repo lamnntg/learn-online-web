5d10ffb010fdff39940ae5f8646a54874f0d1d77
lam nguyen <lamnguyen.hust@gmail.com>
2022-06-22 17:31:51 +0700

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
-export const API_URL = 'http://localhost:8080/api/auth/';
-export const API_ROOT = 'http://localhost:8080/api';
+export const API_URL = 'https://learn-online-app.herokuapp.com/api/auth/';
+export const API_ROOT = 'https://learn-online-app.herokuapp.com/api';
 export const CONVERT_API_SECRET_KEY = '4klYVXNzKFvYWito';
+export const API_SOCKET_URL = '';
 
 export const ROLE_ADMIN = 'ROLE_ADMIN';
 export const ROLE_MODERATOR = 'ROLE_MODERATOR';
